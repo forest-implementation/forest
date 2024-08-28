9a24d3143b9904177236aa509a0527e7b8c486f9
Adam Ulrich <adamo.ulrich@gmail.com>
2024-08-28 11:59:39 +0200

    NOVELTY MAX depth working

--- lib/service/novelty.ex
+++ lib/service/novelty.ex
@@ -3,12 +3,7 @@ defmodule Service.Novelty do
     Enum.at(element, dimension) < sp
   end
 
-  def split_data(%{:data => [a], :ranges => ranges, :depth => d}),
-    do: {%{depth: d, ranges: ranges, data: a}}
-  def split_data(%{:data => [], :ranges => ranges, :depth => d}),
-    do: {%{depth: d, ranges: ranges, data: nil}}
-  def split_data(%{data: data, ranges: ranges, depth: depth}) do
-    # take range for the corresponding dimension (random)
+  def next_split(%{data: data, ranges: ranges, depth: depth}) do
     dimension = Service.RandomSeed.generate_int(0, (data |> hd |> length) - 1)
 
     # lets split range in two .. start,
@@ -20,16 +15,46 @@ defmodule Service.Novelty do
        List.replace_at(ranges, dimension, {sp, max})}
 
     %{true => left_data, false => right_data} =
-      data |> Enum.group_by(&decision(&1, {dimension, sp})) |> Map.merge(%{ :false => [], :true => []}, fn _,v1,_ -> v1 end )
+      data
+      |> Enum.group_by(&decision(&1, {dimension, sp}))
+      |> Map.merge(%{false => [], true => []}, fn _, v1, _ -> v1 end)
 
     {{dimension, sp}, %{data: left_data, ranges: left_ranges, depth: depth + 1},
      %{data: right_data, ranges: right_ranges, depth: depth + 1}}
   end
 
-  def split_data(%{:data => data, :ranges => ranges}), do: split_data(%{:data => data, :ranges => ranges, :depth => 0})
-  def split_data(data, ranges), do: split_data(%{:data => data, :ranges => ranges, :depth => 0})
+  def make_split(max_depth) do
+    fn
+      %{data: [_], depth: _} = dp -> {dp}
+      %{data: [], depth: _} = dp -> {dp}
+      %{data: _, ranges: _, depth: d} = dp when d == max_depth -> {dp}
+      %{data: _, ranges: _, depth: _} = dp -> next_split(dp)
+      %{data: data, ranges: ranges} -> make_split(max_depth).(%{:data => data, :ranges => ranges, :depth => 0})
+    end
+  end
 
   def batch(%{data: data, batch_size: bs} = dp, _) do
     %{dp | data: data |> Enum.take_random(bs)}
   end
+
+  def ecko(depths) do
+    Enum.sum(depths) / length(depths)
+  end
+
+  def harmonic_num(n) do
+    1..n |> Stream.map(&(1.0 / &1)) |> Enum.sum()
+  end
+
+  def average_path_length_c(n) when n < 2, do: 0
+  def average_path_length_c(2), do: 1
+
+  def average_path_length_c(batch_size) do
+    2 * harmonic_num(batch_size - 1) - 2 * (batch_size - 1) / batch_size
+  end
+
+  def anomaly_score(depths, batch_size) do
+    :math.pow(2, -ecko(depths) / average_path_length_c(batch_size))
+  end
+
+
 end
--- test/service/novelty_test.exs
+++ test/service/novelty_test.exs
@@ -1,36 +1,43 @@
 defmodule ServiceNoveltyTest do
   use ExUnit.Case
 
-  test "from_data_Novelty" do
-    :rand.seed(:exsplus, {12245, 67890, 54321})
+  test "make split" do
 
-    assert INode.from_data(%{:data => [[1]], :ranges => [{1, 3}]}, &Service.Novelty.split_data/1) ==
+
+    assert %{data: [[5]], ranges: {-10,10}} |> Service.Novelty.make_split(5).() == {%{data: [[5]], ranges: {-10,10}, depth: 0}}
+
+    assert [[5], [6]] |> Service.Outlier.make_split(5).() ==
+      {{0, 5.811948033218323}, %{data: [[5]], depth: 1}, %{data: [[6]], depth: 1}}
+  end
+
+  test "from_data_Novelty" do
+    assert INode.from_data(%{:data => [[1]], :ranges => [{1, 3}]}, Service.Novelty.make_split(50)) ==
              %INode{
-               carrier: %{data: [1], depth: 0, ranges: [{1, 3}]},
+               carrier: %{data: [[1]], depth: 0, ranges: [{1, 3}]},
                left: nil,
                right: nil
              }
 
     assert INode.from_data(
              %{:data => [[1], [2], [3]], :ranges => [{1, 3}]},
-             &Service.Novelty.split_data/1
+             Service.Novelty.make_split(50)
            ) ==
              %INode{
                carrier: {0, 2},
                left: %INode{
-                 carrier: %{data: [1], depth: 1, ranges: [{1, 2}]},
+                 carrier: %{data: [[1]], depth: 1, ranges: [{1, 2}]},
                  left: nil,
                  right: nil
                },
                right: %INode{
                  carrier: {0, 2.5},
                  left: %INode{
-                   carrier: %{data: [2], depth: 2, ranges: [{2, 2.5}]},
+                   carrier: %{data: [[2]], depth: 2, ranges: [{2, 2.5}]},
                    left: nil,
                    right: nil
                  },
                  right: %INode{
-                   carrier: %{data: [3], depth: 2, ranges: [{2.5, 3}]},
+                   carrier: %{data: [[3]], depth: 2, ranges: [{2.5, 3}]},
                    left: nil,
                    right: nil
                  }
@@ -54,24 +61,26 @@ defmodule ServiceNoveltyTest do
       [90, 10]
     ]
 
+
+    batch_size = length(data)
+
     forest =
       Forest.init(
         50,
-        %{data: data, ranges: [{0, 110}, {-5, 105}], batch_size: length(data)},
-        &Service.Novelty.split_data/1,
+        %{data: data, ranges: [{0, 110}, {-5, 105}], batch_size: batch_size},
+        Service.Novelty.make_split(50),
         &Service.Novelty.batch/2
       )
-      |> IO.inspect()
 
-    forest
-    |> Forest.evaluate([105, 20], &Service.Outlier.decision/2)
-    |> Enum.map(fn %{data: _, depth: d} -> d end)
-    |> IO.inspect()
+      assert forest
+      |> Forest.evaluate([105, 20], &Service.Outlier.decision/2)
+      |> Enum.map(fn %{data: _, depth: d} -> d end)
+      |> Service.Novelty.anomaly_score(batch_size) < 0.5
 
-    forest
-    |> Forest.evaluate([25, 20], &Service.Outlier.decision/2)
-    |> Enum.map(fn %{data: _, depth: d} -> d end)
-    |> IO.inspect()
+      assert forest
+      |> Forest.evaluate([25, 20], &Service.Outlier.decision/2)
+      |> Enum.map(fn %{data: _, depth: d} -> d end)
+      |> Service.Novelty.anomaly_score(batch_size) > 0.6
   end
 
   test "clanek data NOT UNIQUE" do
@@ -88,26 +97,28 @@ defmodule ServiceNoveltyTest do
       [90, 30],
       [90, 20],
       [90, 10],
-      [90, 10],
+      [90, 10]
     ]
+
+
+    batch_size = length(data)
 
     forest =
       Forest.init(
         50,
-        %{data: data, ranges: [{0, 110}, {-5, 105}], batch_size: length(data)},
-        &Service.Novelty.split_data/1,
+        %{data: data, ranges: [{0, 110}, {-5, 105}], batch_size: batch_size},
+        Service.Novelty.make_split(50),
         &Service.Novelty.batch/2
       )
-      |> IO.inspect()
 
-    forest
-    |> Forest.evaluate([105, 20], &Service.Outlier.decision/2)
-    |> Enum.map(fn %{data: _, depth: d} -> d end)
-    |> IO.inspect()
+      assert forest
+      |> Forest.evaluate([105, 20], &Service.Outlier.decision/2)
+      |> Enum.map(fn %{data: _, depth: d} -> d end)
+      |> Service.Novelty.anomaly_score(batch_size) < 0.5
 
-    forest
-    |> Forest.evaluate([25, 20], &Service.Outlier.decision/2)
-    |> Enum.map(fn %{data: _, depth: d} -> d end)
-    |> IO.inspect()
+      assert forest
+      |> Forest.evaluate([25, 20], &Service.Outlier.decision/2)
+      |> Enum.map(fn %{data: _, depth: d} -> d end)
+      |> Service.Novelty.anomaly_score(batch_size) > 0.6
   end
 end
